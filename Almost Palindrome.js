4d0bc7a71cc5bdc68f0f86db1b556c77ea723791
Gabriel Ramirez <gabriel.ramirezdev@gmail.com>
2022-02-20 20:00:00 -0600

    almost palindrome

---
+++ Almost Palindrome.js
@@ -0,0 +1,29 @@
+
+//Palindrome and Almost Palindrome: 
+
+//Time Complexity: O(n)
+//Space Complexity: O(1)
+
+const validSubPalindrome = (s, start, end) => {
+    while (start < end) {
+        if (s[start] !== s[end]) {
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true;
+  };
+
+const validPalindrome = (s) => {
+    let start = 0;
+    let end = s.length - 1;
+    while (start < end) {
+        if (s[start] !== s[end]) {
+            return validSubPalindrome(s, start + 1, end) || validSubPalindrome(s, start, end - 1);
+        }
+        start++;
+        end--;
+    }
+    return true;
+  };
